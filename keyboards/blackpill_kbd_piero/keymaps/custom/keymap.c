5cb047c6d2b370f027b12f6dd56eafcd76339ca0
pier <pierandrea.cancian@gmail.com>
2021-11-06 14:17:12 +0100

    try to flash (?)

--- keyboards/blackpill_kbd_piero/keymaps/custom/keymap.c
+++ keyboards/blackpill_kbd_piero/keymaps/custom/keymap.c
@@ -20,6 +20,7 @@
 
 enum custom_keycodes {
     QMKBEST = SAFE_RANGE,
+    KC_KITE,
     SAVE_EEP,
     KC_ACC,
     KC_SEC,
@@ -63,18 +64,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     KC_LSFT,     KC_Z,     KC_X,     KC_C,    KC_V,        KC_B,    KC_N,   KC_M,      KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,   KC_NO,
 //     KC_LCTL,     KC_LGUI,  KC_LALT,  KC_SEC,  MO(_LOWER),  KC_SPC,  KC_NO,  KC_NO,  KC_SPC,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 [_QWERTY] = LAYOUT_ortho_5x15(
-    KC_GRV,      KC_1,     KC_2,     KC_3,    KC_4,        KC_5,    KC_6,    KC_7,      KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   MO(_LOWER), 
-    KC_TAB,      KC_Q,     KC_W,     KC_E,    KC_R,        KC_T,    KC_LSFT, KC_Y,      KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NO, 
-    KC_ESC,     KC_A,     KC_S,     KC_D,    KC_F,        KC_G,    KC_BSPC, KC_H,      KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_CAPS,  
-    KC_LSFT,     KC_Z,     KC_X,     KC_C,    KC_V,        KC_B,    KC_ENT,  KC_N,      KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,   KC_LGUI,  KC_NO,  
-    KC_LCTL,     KC_SEC,  KC_LGUI,  KC_LALT,  KC_NO,       KC_SPC,  KC_NO,   KC_SPC,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
+    KC_GRV,      KC_1,     KC_2,     KC_3,     KC_4,      KC_5,    KC_6,    KC_7,      KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   MO(_LOWER), 
+    KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,    KC_LSFT, KC_Y,      KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NO, 
+    KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,      KC_G,    KC_BSPC, KC_H,      KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_CAPS,  
+    KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,    KC_ENT,  KC_N,      KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,   KC_LGUI,  KC_NO,  
+    KC_LCTL,     KC_SEC,    KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_NO,   KC_SPC,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
+//               KC_SEC
 //              ^^^^^^^^----sends to second 
 [_SECOND] = LAYOUT_ortho_5x15(
-    KC_NO,   KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
-    KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_F11, KC_F12, KC_P7, KC_P8, KC_P9, KC_PPLS, 
-    KC_CALC, KC_MYCM, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PCMM, 
-    KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, 
-    KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT),
+    KC_GRV,      KC_1,     KC_2,     KC_3,     KC_4,      KC_5,    KC_6,      KC_7,     KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   MO(_LOWER), 
+    KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,    KC_LSFT,   KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NO, 
+    KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,      KC_G,    KC_BSPC,   KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_CAPS,  
+    KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,    KC_ENT,    KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,   KC_LGUI,  KC_NO,  
+    KC_LCTL,     KC_NO,    KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_KITE,   KC_NO,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
+
+// [_SECOND] = LAYOUT_ortho_5x15(
+//     KC_NO,   KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
+//     KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_F11, KC_F12, KC_P7, KC_P8, KC_P9, KC_PPLS, 
+//     KC_CALC, KC_MYCM, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PCMM, 
+//     KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, 
+//     KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT),
 
 // [_RGB] = LAYOUT_ortho_5x15(
 //     RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, 
@@ -147,8 +156,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_SEC:
             if (record->event.pressed) {
-                        //rgb_matrix_mode(RGB_MATRIX_CUSTOM_SECOND);
-                        layer_on(_SECOND);
+                //rgb_matrix_mode(RGB_MATRIX_CUSTOM_SECOND);
+                layer_on(_SECOND);
             } else {
                 //rgb_matrix_mode(sleepmode_before_mode);
                 layer_off(_SECOND);
