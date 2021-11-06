c40aa93ef760e9a8785d261a466464975c99a1a0
pier <pierandrea.cancian@gmail.com>
2021-11-06 22:14:56 +0100

    kc_kite attempt

--- keyboards/blackpill_kbd_piero/keymaps/custom/keymap.c
+++ keyboards/blackpill_kbd_piero/keymaps/custom/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,    KC_LSFT, KC_Y,      KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NO, 
     KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,      KC_G,    KC_BSPC, KC_H,      KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_CAPS,  
     KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,    KC_ENT,  KC_N,      KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,   KC_LGUI,  KC_NO,  
-    KC_LCTL,     KC_SEC,    KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_NO,   KC_SPC,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
+    KC_LCTL,     TG(KC_SEC),   KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_NO,   KC_SPC,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
 //               KC_SEC
 //              ^^^^^^^^----sends to second 
 [_SECOND] = LAYOUT_ortho_5x15(
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,    KC_LSFT,   KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_NO, 
     KC_ESC,      KC_A,     KC_S,     KC_D,     KC_F,      KC_G,    KC_BSPC,   KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,  KC_CAPS,  
     KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,    KC_ENT,    KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,   KC_LGUI,  KC_NO,  
-    KC_LCTL,     KC_NO,    KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_KITE,   KC_NO,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  RESET),
+    KC_LCTL,     KC_NO,    KC_LGUI,  KC_LALT,  KC_SPC,    KC_SPC,  KC_KITE,   KC_NO,    KC_NO,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_RCTL,  KC_NO),
 
 // [_SECOND] = LAYOUT_ortho_5x15(
 //     KC_NO,   KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO, KC_QMK_TLBX, KC_WHATSAPP, KC_NO, KC_NO, KC_TELEGRAM, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_D, 
     KC_NO, KC_NO, KC_SPOTIFY, KC_NO, KC_NO, KC_GOOGLE, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN4, KC_BTN1, KC_MS_U, KC_BTN2, 
-    KC_PWR, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_MS_L, KC_MS_D, KC_NO ),
+    KC_PWR, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_MS_L, KC_MS_D, RESET),
 
 [_RAISE] = LAYOUT_ortho_5x15(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_2, KC_0, KC_MINS, KC_EQL, KC_DEL, KC_PGUP,
@@ -146,9 +146,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case KC_ACC:
             if (record->event.pressed) {
-                        // rgb_matrix_mode(RGB_MATRIX_CUSTOM_my_cool_effect);
-                        SEND_STRING(SS_DOWN(X_LSFT) SS_TAP(X_LALT) SS_UP(X_LSFT));
-                        layer_on(_ACCENTED);
+                // rgb_matrix_mode(RGB_MATRIX_CUSTOM_my_cool_effect);
+                SEND_STRING(SS_DOWN(X_LSFT) SS_TAP(X_LALT) SS_UP(X_LSFT));
+                layer_on(_ACCENTED);
             } else {  // at key release
                 SEND_STRING(SS_DOWN(X_LSFT) SS_TAP(X_LALT) SS_UP(X_LSFT));
                 layer_off(_ACCENTED);
@@ -162,6 +162,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 //rgb_matrix_mode(sleepmode_before_mode);
                 layer_off(_SECOND);
             }
+            break;
+        case KC_KITE:
+            if (record->event.pressed) {
+                register_code(KC_X);
+                register_code(KC_V);
+                register_code(KC_C);
+            } else {
+                unregister_code(KC_X);
+                unregister_code(KC_V);
+                unregister_code(KC_C);            }
             break;
         }
     return true;
